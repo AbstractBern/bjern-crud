4f3921aaa6e50c8d54a5c0c482ef12a53b26853a
rob2radical <42249588+rob2radical@users.noreply.github.com>
2021-08-11 18:25:33 -0500

    Add files via upload

---
+++ config/db.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const mysql = require('mysql');
+
+var con = mysql.createConnection({ 
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'group12'
+});
+
+con.connect(function(err) { 
+        if (err) throw err;
+        console.log("Connected to MySQL!");
+});
+
+module.exports = con;
+
