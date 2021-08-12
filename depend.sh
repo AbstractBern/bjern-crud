c2bd9472b813ac04a97aa88e1f1ee99b4880dd54
rob2radical <42249588+rob2radical@users.noreply.github.com>
2021-08-11 19:44:54 -0500

    Delete depend.sh

--- depend.sh
+++
@@ -1,5 +0,0 @@
-mkdir foodies
-cd foodies
-npm init --yes
-npm install @okta/jwt-verifier@1.0.0 @okta/oidc-middleware@2.0.0 body-parser@1.19.0 dotenv@8.0.0 express@4.17.1 express-session@1.16.2 express-with-json@0.0.6 glob@7.1.4 mysql@2.17.1 reflect-metadata@0.1.13 ts-node@8.3.0 typeorm@0.2.18 typescript@3.5.3
-npx ts-typie
