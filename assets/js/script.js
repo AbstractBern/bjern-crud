ecf13863cc716a7d9ee370cc28ec9ba47ecb538b
Nicolas Ruiz <syngenruiz2@gmail.com>
2021-08-11 21:45:30 -0500

    progress update

--- assets/js/script.js
+++
@@ -1,5 +0,0 @@
-/* Here's an example script! You can include anything you'd like to include
-on your webpage in here! Remove the "//" before the alert to have it run
-next time you go to the page! */
-
-//alert("Woah what?!? An Alert!!")
